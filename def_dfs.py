cfb6c0354c64157854ea4e6433ddddb8f3c7af25
suzinee <ggaebong.u@gmail.com>
2020-10-09 00:50:46 +0900

    Let's get ready for coding tests

---
+++ def_dfs.py
@@ -0,0 +1,28 @@
+"""
+20201009
+Exercise shadowing the code;
+A Definition of Depth-First Search
+from https://github.com/ndb796/python-for-coding-test/blob/master/5/9.py
+"""
+def dfs(graph, v, visited):
+  visited[v] = True
+  print(v, end=' ')
+  for i in graph[v]:
+    if not visited[i]:
+      dfs(graph, i, visited)
+
+graph = [
+  [],
+  [2,3,8],
+  [1,7],
+  [1,4,5],
+  [3,5],
+  [3,4],
+  [7],
+  [2,6,8],
+  [1,7]
+]
+
+visited = [False] * 9
+
+dfs(graph, 1, visited)
